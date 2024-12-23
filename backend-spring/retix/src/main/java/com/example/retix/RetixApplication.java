0e3868e84022926859c1f661e1b426624dd8871d
Bhavitha M <b44068845@gmail.com>
2024-12-23 12:09:28 +0530

    remove old backend

--- backend-spring/retix/src/main/java/com/example/retix/RetixApplication.java
+++ backend-spring/retix/src/main/java/com/example/retix/RetixApplication.java
@@ -3,10 +3,11 @@ package com.example.retix;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication  // This annotation is a combination of @Configuration, @EnableAutoConfiguration, and @ComponentScan
+@SpringBootApplication
 public class RetixApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RetixApplication.class, args);  // Start the Spring Boot application
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RetixApplication.class, args);
+	}
+
 }
