a742861ca05d6d5b078f20f068baac19310fb993
Bhavitha M <b44068845@gmail.com>
2024-12-25 23:24:41 +0530

    Create extra controller
    [] - fix update controller

--- backend-spring/retix/src/main/java/com/example/retix/service/UserService.java
+++ backend-spring/retix/src/main/java/com/example/retix/service/UserService.java
@@ -2,8 +2,11 @@ package com.example.retix.service;
 
 import com.example.retix.model.User;
 import com.example.retix.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,4 +18,29 @@ public class UserService {
     public User createUser(User user) {
         return userRepository.save(user); // Saves the new user into the database
     }
+
+    public User findUserById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    public User updateUser(User user) {
+        // Check if the user with the given id exists
+        Optional<User> existingUserOptional = userRepository.findById(user.getId());
+        if (existingUserOptional.isPresent()) {
+            User existingUser = existingUserOptional.get();
+
+            // Update the necessary fields
+            existingUser.setEmail(user.getEmail());
+            existingUser.setFullName(user.getFullName());
+            existingUser.setActive(user.isActive());
+            existingUser.setPassword(user.getPassword());
+            existingUser.setUsername(user.getUsername());
+
+            return userRepository.save(existingUser);
+        } else {
+            throw new EntityNotFoundException("User not found with id: " + user.getId());
+        }
+    }
+
+
 }
