dacd21b18cf7997a20c930e5d2fe48baee8ab0df
nikitha-56 <nikithatalluri1@gmail.com>
2024-12-26 17:38:50 +0530

    added something in securyconfig file

--- backend-spring/retix/src/main/java/com/example/retix/model/Ticket.java
+++ backend-spring/retix/src/main/java/com/example/retix/model/Ticket.java
@@ -1,10 +1,7 @@
 package com.example.retix.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column;
 import java.time.LocalDateTime;
 
 
